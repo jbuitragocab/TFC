a720b39668d520187dd503a5faeedf6f11f29b7f
Javier <javierbc2223@gmail.com>
2025-05-16 15:56:15 +0200

    Reorganización de rutas (prueba)

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {return view('welcome');})->middleware('auth')->name('home');
+
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+Route::get('/logout', function () {auth()->logout();return redirect('/login');})->name('logout');
 
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/index', function () {return view('index');})->middleware('auth')->name('index');
-Route::get('/home', function () {return view('welcome');})->middleware('auth')->name('home');
-Route::get('/logout', function () {auth()->logout();return redirect('/login');})->name('logout');
 Route::get('/contact', function () {return view('contact');})->middleware('auth')->name('contact');
 Route::post('contact', [ContactController::class, 'sendEmail'])->name('contact.send');
 Route::get('/restaurantes', [RestauranteController::class, 'index'])->name('restaurantes.index');
