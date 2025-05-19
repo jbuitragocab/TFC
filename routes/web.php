2afab0e54e31d3d61f072cb40a7b0a1dad93fdec
Javier <javierbc2223@gmail.com>
2025-05-19 16:25:25 +0200

    Solucionado porblemas de login cuando eres superuser

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,19 @@ Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->na
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/index', function () {return view('index');})->middleware('auth')->name('index');
-Route::get('/contact', function () {return view('contact');})->middleware('auth')->name('contact');
+Route::get('/contact', function () {return view('contact');})->middleware('auth')->name('contact')->middleware('auth');
 Route::post('contact', [ContactController::class, 'sendEmail'])->name('contact.send');
-Route::get('/restaurantes', [RestauranteController::class, 'index'])->name('restaurantes.index');
+Route::get('/restaurantes', [RestauranteController::class, 'index'])->name('restaurantes.index')->middleware('auth');
 Route::get('/admin', [LoginController::class, 'adminIndex'])->name('admin.index')->middleware('auth');
 
 
-Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
-    Route::get('/index', [AdminController::class, 'index'])->name('admin.index');
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/index', [AdminController::class, 'index'])->name('index');
     Route::get('/create', [AdminController::class, 'create'])->name('create');
     Route::post('/', [AdminController::class, 'store'])->name('store');
     Route::delete('/{restaurante}', [AdminController::class, 'destroy'])->name('destroy');
-    Route::get('/{restaurante}/edit', [AdminController::class, 'edit'])->name('admin.edit');
-    Route::put('/{restaurante}', [AdminController::class, 'update'])->name('admin.update');
+    Route::get('/{restaurante}/edit', [AdminController::class, 'edit'])->name('edit');
+    Route::put('/{restaurante}', [AdminController::class, 'update'])->name('update');
     Route::get('/{restaurante}', [AdminController::class, 'show'])->name('show');
 });
+
